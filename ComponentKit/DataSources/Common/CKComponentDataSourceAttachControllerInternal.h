a2360e121c2459520885de443f941260b9b6cb98
Conrad Kramer <conrad@conradkramer.com>
2017-01-31 17:02:14 -0800

    Make CKComponentDataSourceAttachController header modular

--- ComponentKit/DataSources/Common/CKComponentDataSourceAttachControllerInternal.h
+++ ComponentKit/DataSources/Common/CKComponentDataSourceAttachControllerInternal.h
@@ -8,7 +8,9 @@
  *
  */
 
-#import <Foundation/Foundation.h>
+#import <ComponentKit/CKComponentDataSourceAttachController.h>
+#import <ComponentKit/CKComponentScopeTypes.h>
+#import <ComponentKit/CKComponentLayout.h>
 
 /** This is exposed for unit tests. */
 @interface CKComponentDataSourceAttachState : NSObject
